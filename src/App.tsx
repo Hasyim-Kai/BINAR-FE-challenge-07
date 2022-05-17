04596251c3b46c049dadab0fc3af42528738084f
ichihatsu1 <ichihatsu1@gmail.com>
2022-05-17 09:53:21 +0700

    Remove Redux Persist

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,9 @@
 import { Provider } from "react-redux";
-import { PersistGate } from "redux-persist/integration/react";
-import { persistor, store } from "./redux/store";
+import store from "./redux/store";
 import AppRoutes from "./Routes";
 
 export default function App() {
   return <Provider store={store}>
-    <PersistGate loading={null} persistor={persistor}>
       <AppRoutes />
-    </PersistGate>
   </Provider>
 }
