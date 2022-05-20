52974fd12ebdf79af2cfdae40727d611f452bcfb
ichihatsu1 <ichihatsu1@gmail.com>
2022-05-20 12:26:27 +0700

    Refctor: add Additional Routing

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,8 +1,12 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
+import AddNewCar from "./pages/AddNewCar";
 import CarDetailPage from "./pages/CarDetailPage";
+import CarList from "./pages/CarList";
+import AdminDashboard from "./pages/AdminDashboard";
 import HomePage from "./pages/HomePage";
+import InvoicePage from "./pages/InvoicePage";
 import NotFound from "./pages/NotFound";
 import SearchPage from "./pages/SearchPage";
 import SearchResultPage from "./pages/SearchResultPage";
@@ -13,9 +17,19 @@ export default function AppRoutes() {
       <main>
          <Routes>
             <Route index element={<HomePage />} />
+            <Route path="home" element={<HomePage />} />
             <Route path="search" element={<SearchPage />} />
             <Route path="search-result" element={<SearchResultPage />} />
-            <Route path="car-rent-detail/:id" element={<CarDetailPage />} />
+            <Route path="car-rent-detail">
+               <Route path=":id" element={<CarDetailPage />} />
+               <Route path=":id/invoice" element={<InvoicePage />} />
+            </Route>
+            <Route path="admin-dashboard" element={<AdminDashboard />} />
+            <Route path="admin-cars">
+               <Route index element={<CarList />} />
+               <Route path=":isAddCarSuccess" element={<CarList />} />
+            </Route>
+            <Route path="admin-cars/addCar" element={<AddNewCar />} />
             <Route path='*' element={<NotFound />} />
          </Routes>
       </main>
